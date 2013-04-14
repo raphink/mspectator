117229d2408fa869eeecfa4bea60c68ac8c58e57
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-14 09:44:42 +0200

    Join with a comma

--- lib/mspectator/matchers/have_package.rb
+++ lib/mspectator/matchers/have_package.rb
@@ -5,10 +5,10 @@ RSpec::Matchers.define :have_package do |package|
   end
 
   failure_message_for_should do |actual|
-    "expected that all hosts would have the #{expected} package, but found hosts without it:\n  #{@failed.join('  \n')}"
+    "expected that all hosts would have the #{expected} package, but found hosts without it: #{@failed.join(', ')}"
   end
 
   failure_message_for_should_not do |actual|
-    "expected that no hosts would have the #{expected} package, but found hosts with it:\n  #{@passed.join('  \n')}"
+    "expected that no hosts would have the #{expected} package, but found hosts with it: #{@passed.join(', ')}"
   end
 end
