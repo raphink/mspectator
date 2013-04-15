9eb3a477ed292fb24f4a06d28d21a3d62035c86c
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-15 12:54:33 +0200

    Use hash for pass_puppet_spec matcher

--- lib/mspectator/matchers/pass_puppet_spec.rb
+++ lib/mspectator/matchers/pass_puppet_spec.rb
@@ -2,21 +2,24 @@ RSpec::Matchers.define :pass_puppet_spec do
   match do
     spec_mc = rpcclient('spec')
     spec_mc.progress = false
-    # TODO: use hashes to grab output
     @passed = []
-    @failed = []
+    @failed = {}
     spec_mc.run.each do |resp|
       if resp[:data][:passed]
         @passed << resp[:sender]
       else
-        @failed << resp[:sender]
+        @failed[resp[:sender]] = resp[:data][:output]
       end
     end
     @failed.empty?
   end
 
   failure_message_for_should do |actual|
-    "expected that all hosts would pass tests, the following didn't: #{@failed.join(', ')}"
+    text = "expected that all hosts would pass tests, the following didn't:\n"
+    @failed.each do |h, o|
+      text += "#{h}:\n #{o}"
+    end
+    text
   end
 
   failure_message_for_should_not do |actual|
